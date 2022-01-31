6bd7acbbe19d29b8352ce47f1ff2a066f352ddb7
Waayn <ronanbeaufort@live.fr>
2022-01-31 10:36:47 +0100

    api prepared
    API prepared / create-react-app in front

---
+++ backend/database/models/Pokemon.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+export default class Pokemon {
+    constructor(uri) {
+        this.uri = uri;
+    }
+
+    async connect() {
+        await mongoose.connect(this.uri, { useNewUrlParser: true, useUnifiedTopology: true })
+        this.pokemonSchema = new mongoose.Schema({
+            id: Number,
+            name: Object,
+            type: Array,
+            base: Object,
+            species: String,
+            description: String,
+            evolution: Object,
+            profile: Object,
+            image: Object,
+            sprite: String,
+            thumbnail: String,
+            hires: String
+        })
+    }
+
+    async getAllPokemons() {
+        const Pokemons = mongoose.model('pokemons', this.pokemonSchema)
+        return new Promise(async (resolve, reject) => {
+            try {
+                const datas = await Pokemons.find()
+                resolve(datas)
+            } catch (err) {
+                reject({ error: "Database connection error" })
+            }
+        })
+    }
+
+    async getPokemonById(pokemonId) {
+        const Pokemons = mongoose.model('pokemons', this.pokemonSchema)
+        return new Promise(async (resolve, reject) => {
+            try {
+                const datas = await Pokemons.find({ id: parseInt(pokemonId) })
+                resolve(datas)
+            } catch (err) {
+                reject({ error: "Database connection error" })
+            }
+        })
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,71 @@
 import express from "express";
 import dotenv from "dotenv";
-import MongoDB from "./database/mongoDB.js";
+import cors from "cors";
+import Pokemon from "./database/models/Pokemon.js";
+import User from "./database/models/User.js";
 
 dotenv.config()
 const PORT = process.env.PORT
 const URI = process.env.DB_HOST
+const SALT = process.env.SALT
 
 const app = express()
-const mongoDB = new MongoDB(URI)
-mongoDB.connect()
+const pokemonDB = new Pokemon(URI)
+pokemonDB.connect()
+const userDB = new User(URI, SALT)
+userDB.connect()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
 
+//------------------------  USER  ------------------------
+
+app.post('/create/user', (req, res) => {
+    if (!req.body.email || !req.body.password || !req.body.username) {
+        return res.status(400).json({ message: 'Error. Please enter a username, a email and a password' })
+    }
+    userDB.createUser({ username: req.body.username, password: req.body.password, email: req.body.email })
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(500).json({ error }))
+})
+
+app.post('/login', (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({ message: 'Error. Please enter a username and a password' })
+    }
+    userDB.login({ email: req.body.email, password: req.body.password })
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(500).json(error))
+})
+
+app.post('/get/user', (req, res) => {
+    if (!req.body.id) {
+        return res.status(400).json({ message: 'Error. Please enter an id' })
+    }
+    userDB.getUserById(req.body.id)
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(500).json(error))
+})
+
+//------------------------  POKEMON  ------------------------
+
+
 app.get('/pokemons', (req, res) => {
-    mongoDB.getAllPokemons()
-        .then(pokemons => res.json(pokemons))
-        .catch(error => res.json({ error }))
+    pokemonDB.getAllPokemons()
+        .then(pokemons => res.status(200).json(pokemons))
+        .catch(error => res.status(500).json(error))
 })
 
 app.get('/pokemon/:pokemonId', (req, res) => {
-    mongoDB.getPokemonById(req.params.pokemonId)
-        .then(pokemon => res.json(pokemon))
-        .catch(error => res.json({ error }))
+    if (!req.body.id) {
+        return res.status(400).json({ message: 'Error. Please enter an id' })
+    }
+    pokemonDB.getPokemonById(req.body.id)
+        .then(pokemon => res.status(200).json(pokemon))
+        .catch(error => res.status(500).json(error))
 })
